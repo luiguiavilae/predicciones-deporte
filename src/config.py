facac347cd58cf1f4d0ebc10f1d48f8e30549271
Luiguiavilae <luiguiavilae@gmail.com>
2026-01-17 21:54:51 -0500

    Add LaLiga (SP1) and retrain models

--- src/config.py
+++ src/config.py
@@ -7,8 +7,9 @@ class LeagueConfig:
     seasons: list[str]
 
 LEAGUES = [
-    LeagueConfig(code="EPL", fd_code="E0", seasons=["2526", "2425", "2324", "2223", "2122"]),
-    LeagueConfig(code="SA",  fd_code="I1", seasons=["2526", "2425", "2324", "2223", "2122"]),
+    LeagueConfig(code="EPL", fd_code="E0",  seasons=["2526", "2425", "2324", "2223", "2122"]),
+    LeagueConfig(code="SA",  fd_code="I1",  seasons=["2526", "2425", "2324", "2223", "2122"]),
+    LeagueConfig(code="LL",  fd_code="SP1", seasons=["2526", "2425", "2324", "2223", "2122"]),  # LaLiga
 ]
 
 FOOTBALL_DATA_BASE = "https://www.football-data.co.uk/mmz4281"
