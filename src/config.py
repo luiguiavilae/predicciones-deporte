77d75e347ac90d8a32543a4edf604a9a591d6151
Luiguiavilae <luiguiavilae@gmail.com>
2026-01-20 16:29:56 -0500

    Add UEFA Champions League (EC) to data pipeline

--- src/config.py
+++ src/config.py
@@ -9,7 +9,10 @@ class LeagueConfig:
 LEAGUES = [
     LeagueConfig(code="EPL", fd_code="E0",  seasons=["2526", "2425", "2324", "2223", "2122"]),
     LeagueConfig(code="SA",  fd_code="I1",  seasons=["2526", "2425", "2324", "2223", "2122"]),
-    LeagueConfig(code="LL",  fd_code="SP1", seasons=["2526", "2425", "2324", "2223", "2122"]),  # LaLiga
+    LeagueConfig(code="LL",  fd_code="SP1", seasons=["2526", "2425", "2324", "2223", "2122"]),
+    LeagueConfig(code="UCL", fd_code="EC", seasons=["2425", "2324"]
+),
+
 ]
 
 FOOTBALL_DATA_BASE = "https://www.football-data.co.uk/mmz4281"
